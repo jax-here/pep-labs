e9f96b6efb53a62ea9fa61bda20f5f98fa700604
Jax <aapollon@nyit.edu>
2023-01-06 17:50:48 +0000

    Saved progress on ArraySum 2023-01-06 17:50:48 +0000

--- ArraySum/src/main/java/ArraySum.java
+++ ArraySum/src/main/java/ArraySum.java
@@ -7,7 +7,11 @@ public class ArraySum {
      * @return the sum of all values in arr.
      */
     public int sum(int[] arr){
+        int Sum=0;
+        for(int i=0; i<arr.length; i++){
+            Sum = Sum + arr[i];
+        }
 
-        return 0;
+        return Sum;
     }
 }
